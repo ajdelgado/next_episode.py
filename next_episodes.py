803536225eb5b82049c6ff69a067212956b5a887
antoniojdelgado <antonio.delgado@greenpeace.org>
2015-09-27 16:52:06 +0200

    Initial

---
+++ next_episodes.py
@@ -0,0 +1,314 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+########################################################################
+##
+## next_episode.py
+## Antonio J. Delgado 2013 antonio@susurrando.com
+##
+## Description: Find the last downloaded episodes and find the next.
+## Usage: Create the folder in the PATH variable, inside create a folder
+## for each TV show (replace spaces with dots to improve the search),
+## and the script will search for the downloaded episodes and download
+## the next one using Transmission.
+########################################################################
+import sys,os,time,re,urllib2,string,tempfile, gzip
+import psutil
+
+PATH="/home/ficheros/videos/series/"
+#PATHTORRENTS="/home/ficheros/torrents/"
+DEBUG=0
+VERSION=0.1
+PROXY=""
+TRANSMISSIONUSER="admin"
+TRANSMISSIONPASS=""
+TRANSMISSIONSERVER="localhost"
+TRANSMISSIONPORT=6884
+os.environ['http_proxy']=PROXY
+USER_AGENT="ultimos_episodios.py/v%s" % (VERSION)
+LOGFILE="%s/log/ultimos_episodios.log" % os.environ['HOME']
+#EXCEPTIONS={"Person.of.Interest", "Arrow", "Better.Call.Saul","The.Knick","Star.Wars.The.Clone.Wars", "True.Blood","Green.Wing","Vikings","Community", "Black.Mirror", "The.Americans", "An.Idiot.Abroad", "Dexter", "Futurama", "Breaking.Bad", "Californication", "Spartacus","Dominion.Tank.Police","BAND.OF.BROTHERS","House","Chuck","How.I.Met.Your.Mother","Mythbuster","Planet.Earth","The.Twilight.Zone","Yes.Prime.Minister","Doctor.Who","The.Wire"}
+EXCEPTIONS={"The.Flas", "Sense8", "The.Crazy.Ones", "Hunted", "Halt.and.Catch.Fire","Community","Alphas","Person.of.Interest","BAND.OF.BROTHERS","Arrow","Green.Wing","Black.Mirror","Vikings","Californication","Fargo","Spartacus","Revolution.2012","Dexter","The.Americans","Breaking.Bad","Better.Call.Saul","The.Killing","An.Idiot.Abroad","The.Knick","Utopia","Orange.Is.The.New.Black","Star.Wars.The.Clone.Wars"}
+URLBASE="https://kat.ph/usearch"
+def Message(TEXT,NIVEL=0):
+	global DEBUG,LOGFILE
+	if DEBUG >= NIVEL:
+		print TEXT
+	LOGPATH=os.path.dirname(LOGFILE)
+	if not os.path.exists(LOGPATH):
+		if DEBUG>0:
+			print "Creating folder for log file '%s'" % LOGPATH
+		os.mkdir(LOGPATH,0700)
+	LOGF=open(LOGFILE,"w")
+	LOGF.write("%s\n" % TEXT)
+	LOGF.close()
+
+def CheckIfRunning():
+	for process in psutil.get_pid_list():
+		if process != os.getpid():
+			try:
+				ps=psutil.Process(process)
+				try:
+					cmdline=ps.cmdline
+					if len(cmdline)>1:
+						if cmdline[1] == sys.argv[0]:
+							Message("Already running process %s '%s'" % (process,cmdline))
+							sys.exit(1)
+				except psutil.AccessDenied:
+					Message("I can't read information for process %s" % process)
+			except psutil.NoSuchProcess:
+				Nothing=0
+def GetArguments():
+	global DEBUG
+	for arg in sys.argv:
+		if arg == "-v" or arg == "-d" or arg == "--d" or arg == "--v":
+			DEBUG=DEBUG + 1
+			Message("III Increasing debuf level to %s" % DEBUG)
+def RecursiveFileListing(PATH):
+	if os.path.exists(PATH) == False:
+		return False
+	FILES=[]
+	for RAIZ, CARPETAS, SFILES in os.walk(PATH,followlinks=True):
+		for FILE in SFILES:
+			FILES.append(os.path.join(RAIZ,FILE))
+	#for RAIZ, CARPETAS, SFILES in os.walk("/home/ficheros/incoming",followlinks=True):
+		#for FILE in SFILES:
+			#FILES.append(os.path.join(RAIZ,FILE))
+	return FILES
+def AddMagnet(URL):
+	import transmissionrpc
+	global TRANSMISSIONUSER,TRANSMISSIONPASS,TRANSMISSIONSERVER,TRANSMISSIONPORT
+	conn=transmissionrpc.Client(TRANSMISSIONSERVER, user=TRANSMISSIONUSER,password=TRANSMISSIONPASS,port=TRANSMISSIONPORT)
+	conn.add_torrent(URL)
+def LastShowEpisode(SHOW):
+	global PATH
+	FILES=RecursiveFileListing("%s/%s" % (PATH,SHOW))
+	LASTEPISODE=0
+	for FILE in FILES:
+		Message("III Checking file '%s'" % FILE,2)
+		EPISODE=0
+		m=re.match(r".*[Ss](?P<SEASON>[0-9]{1,2})[Ee](?P<EPISODE>[0-9]{1,2}).*",FILE)
+		if m != None:
+			RESULT=m.groupdict()
+			EPISODE=int(RESULT['SEASON'])*100+int(RESULT['EPISODE'])
+		else:
+			m=re.match(r".*(?P<SEASON>[0-9]{1,2})x(?P<EPISODE>[0-9]{1,2}).*",FILE)
+			if m != None:
+				RESULT=m.groupdict()
+				EPISODE=int(RESULT['SEASON'])*100+int(RESULT['EPISODE'])
+			else:
+				EPISODE=0
+		if EPISODE>LASTEPISODE:
+			LASTEPISODE=EPISODE
+	if LASTEPISODE == 0:
+		Message("III There is no downloaded episodes of the show '%s' or it was the '%s'" % (SHOW,LASTEPISODE))
+	else:
+		Message("III Last episode downloaded from show '%s' was '%s'" % (SHOW,LASTEPISODE))
+	return LASTEPISODE
+def SearchNextEpisode(SHOW,LASTEPISODECOMPLETO):
+	LASTSEASON="%s" % (int(round(LASTEPISODECOMPLETO/100)))
+	LASTSEASON=string.rjust(LASTSEASON,2,"0")
+	Message("Last season = %s" % LASTSEASON,3)
+	LASTEPISODE="%s" % int(LASTEPISODECOMPLETO-(int(LASTSEASON)*100))
+	LASTEPISODE=string.rjust(LASTEPISODE,2,"0")
+	Message("Last episode = %s" % LASTEPISODE,3)
+	NEXTEPISODE=int(LASTEPISODE)+1
+	NEXTEPISODE="S%sE%s" % (LASTSEASON,string.rjust("%s" % NEXTEPISODE,2,"0"))
+	
+	NEXTSEASON=string.rjust("%s" % (int(LASTSEASON)+1),2,"0")
+	ANOTHERNEXTEPISODE="S%sE01" % NEXTSEASON
+	Message("III Possible next episodes are '%s' and '%s'" % (NEXTEPISODE,ANOTHERNEXTEPISODE))
+	URL="%s/%s.%s/" % (URLBASE,SHOW,NEXTEPISODE)
+	CONTENT=GetURLContent(URL)
+	NEXTREALEPISODE="0000"
+	if CONTENT == False or "did not match any documents" in CONTENT:
+		URL="%s/%s.%s/" % (URLBASE,SHOW,ANOTHERNEXTEPISODE)
+		CONTENT=GetURLContent(URL)
+		if CONTENT == False or "did not match any documents" in CONTENT:
+			Message("WWW I couldn't find any episode available for '%s'" % SHOW)
+		else:
+			NEXTREALEPISODE=ANOTHERNEXTEPISODE
+	else:
+		NEXTREALEPISODE=NEXTEPISODE
+	Message("III Found next episode '%s'" % NEXTREALEPISODE)
+	return CONTENT,NEXTREALEPISODE
+def timeout(func, args=(), kwargs={}, timeout_duration=1, default=None):
+    import threading
+    class InterruptableThread(threading.Thread):
+        def __init__(self):
+            threading.Thread.__init__(self)
+            self.result = None
+        def run(self):
+            try:
+                self.result = func(*args, **kwargs)
+            except:
+                self.result = default
+    it = InterruptableThread()
+    it.start()
+    it.join(timeout_duration)
+    if it.isAlive():
+        return default
+    else:
+        return it.result
+def SaveTmpFile(content):
+	global TMPFILE
+	import tempfile
+	TMPFILEO=tempfile.mkstemp(prefix='tmp_', dir="/tmp/")
+	TMPFILENAME=TMPFILEO[1]
+	Message("Saving temporarily to '%s' (don't forget to remove it)" % (TMPFILENAME))
+	TMPFILE=open(TMPFILENAME,"w")
+	try:
+		TMPFILE.write(content)
+	finally:
+		TMPFILE.close()
+	return TMPFILENAME
+def GetURLContent(URL):
+	global USER_AGENT
+	import gc,random,urllib2,zipfile,socket,httplib,magic,gzip,cookielib
+	gc.collect()
+	try:
+		PROXY=os.environ["http_proxy"]
+	except:
+		PROXY=""
+	if PROXY != "":
+		proxy = urllib2.ProxyHandler({'http': os.environ["http_proxy"]})
+		opener = urllib2.build_opener(proxy,urllib2.HTTPCookieProcessor(cookielib.CookieJar()))
+	else:
+		opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar()))
+	opener.addheaders = [('User-agent', USER_AGENT)]
+	try:
+		furl=opener.open(URL)
+	except urllib2.URLError, e:
+		Message("Error opening URL '%s'. %s" % (URL,e))
+		return False
+	except urllib2.HTTPError, e:
+		Message("Error opening URL '%s'. %s" % (URL,e))
+		return False
+	except:
+		Message("Unknown error opening URL '%s'" % (URL))
+		return False		
+	try:
+		content=furl.read()
+	except socket.timeout:
+		Message("Error while reading URL '%s'" % (URL))
+		return False
+	except httplib.IncompleteRead:
+		Message("Error while reading URL '%s'" % (URL))
+		return False
+	furl.close()
+	TMPFILEN=SaveTmpFile(content)
+	if zipfile.is_zipfile(TMPFILEN):
+		Message("Content of the URL is a ZipFile")
+		ZFILE=zipfile.ZipFile(TMPFILEN,"r")
+		LZFILES=ZFILE.namelist()
+		for FILE in LZFILES:
+			Message("File in zip: '%s'" % FILE)
+		ZFILE.close()
+		if not cfg['debug']:
+			os.remove(TMPFILEN)
+		return ""
+	else:
+		mag=magic.open(magic.NONE)
+		mag.load()
+		contenttype=mag.buffer(content)
+		if contenttype != None:
+			Message("Downloaded a '%s' file" % contenttype)
+			if contenttype[0:20] == "gzip compressed data":
+				GFILE=gzip.open(TMPFILEN,"r")
+				try:
+					newcontent=GFILE.read()
+				finally:
+					GFILE.close()
+				content=newcontent
+			Message("Obtained %s bytes from '%s'" % (len(content),URL))
+			if DEBUG:
+				SaveTmpFile(content)
+			else:
+				os.remove(TMPFILEN)
+			return content
+		else:
+			if not cfg['debug']:
+				os.remove(TMPFILEN)
+			return False
+def EpisodioDecimal2Par(EPISODEDECIMAL):
+	DECIMALEPISODENUMBER=int(EPISODEDECIMAL.replace("S","").replace("E",""))
+	SEASON=int(DECIMALEPISODENUMBER)/100
+	EPISODE=DECIMALEPISODENUMBER-(SEASON*100)
+	return string.rjust("%s" % SEASON,2,"0"),string.rjust("%s" % EPISODE,2,"0")
+def ExpandGZIP(GZIPDATA):
+	if not GZIPDATA:
+		return False
+	TMPFILEHS,TMPFILE=tempfile.mkstemp(".gz")
+	Message("Expanding data from file '%s'" % TMPFILE)
+	TMPFILEH=open(TMPFILE,"w")
+	TMPFILEH.write(GZIPDATA)
+	TMPFILEH.close()
+	UNZIPFILE= gzip.open(TMPFILE, 'rb')
+	CONTENT = UNZIPFILE.read()
+	UNZIPFILE.close()
+	return CONTENT
+def Checks():
+	global PATH, PATHTORRENTS
+	if not os.path.exists(PATH):
+		Message("Path '%s' doens't exist, exiting." % PATH,1)
+		return False
+	#if not os.path.exists(PATHTORRENTS):
+		#Message("La ruta '%s' no existe, saliendo." % PATHTORRENTS,1)
+		#return False
+	return True
+CheckIfRunning()
+GetArguments()
+
+if not Checks():
+	sys.exit(1)
+SHOWS=os.listdir(PATH)
+for SHOW in SHOWS:
+	EXCEPTION=False
+	for ESHOW in EXCEPTIONS:
+		if ESHOW==SHOW:
+			EXCEPTION=True
+	if not EXCEPTION:
+		Message("IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII")
+		Message("III Checking TV show %s" % SHOW)
+		LASTEPISODE=LastShowEpisode(SHOW)
+		CONTENT,NEXTEPISODE=SearchNextEpisode(SHOW,LASTEPISODE)
+		if CONTENT == False or CONTENT.find("did not match any documents")>-1:
+			Message("III I couldn't find the next episode after '%s'" % LASTEPISODE,2)
+		else:
+			#CONTENT=ExpandGZIP(CONTENT)
+			if len(CONTENT)==0:
+				Message("EEE No data obtained in the search")
+			else:
+				TMPFILEHS,TMPFILE=tempfile.mkstemp()
+				TMPFILEH=open(TMPFILE,"w")
+				TMPFILEH.write(CONTENT)
+				TMPFILEH.close()
+				Message("III Saved as '%s'" % TMPFILE)
+				#m=re.search(r'href="http([][a-zA-Z0-9?=/\.]+\.torrent[][a-zA-Z0-9?=/\.]+)"',CONTENT)
+				#m=re.search(r'href="(http.*\.torrent[][a-zA-Z0-9?=\.]+)',CONTENT)
+				m=re.search(r'href="(magnet:\?[0-9a-zA-Z\&\=\+\%\:\.]*)"',CONTENT)
+				if m != None:
+					TORRENTURL="%s" % m.groups(0)[0]
+					Message("III Found magnet URL '%s'" % TORRENTURL)
+					#CONTENTTORRENT=GetURLContent(TORRENTURL)
+					FNEXTEPISODE="S%sE%s" % (EpisodioDecimal2Par(NEXTEPISODE))
+					#NOMBRETORRENT="%s/%s.%s.torrent" % (PATHTORRENTS,SHOW,FNEXTEPISODE)
+					#if os.path.exists(NOMBRETORRENT):
+						#try:
+							#os.remove(NOMBRETORRENT)
+						#except IOError,e:
+							#Message("EEE Error de entrada y salida eliminando torrent existente '%s'. %s" % (NOMBRETORRENT,e))
+						#except:
+							#Message("EEE Error eliminando torrent existente '%s'" % NOMBRETORRENT)
+					#try:
+						#TORRENT=open("%s" % NOMBRETORRENT,"w")
+						#TORRENT.write(CONTENTTORRENT)
+						#TORRENT.close()
+						#os.chmod("%s" % NOMBRETORRENT,0777)
+						#Message("III Guardado torrent '%s'" % NOMBRETORRENT)
+					#except IOError,e:
+						#Message("EEE Error de entrada y salida guardando torrent '%s'. %s" % (NOMBRETORRENT,e))
+					#except:
+						#Message("EEE Error guardando torrent '%s'" % NOMBRETORRENT)
+					AddMagnet(TORRENTURL)
+				else:
+					Message("EEE I couldn't find any torrent in the search")
